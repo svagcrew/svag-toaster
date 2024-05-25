d4bca79c4fb92c7baf339443764227cb3bcee2b6
Sergei Dmitriev <iserdmi@gmail.com>
2024-05-25 20:29:55 +0400

    Small fix

---
+++ src/index.tsx
@@ -0,0 +1,53 @@
+import type React from 'react'
+import type { ToasterProps, ToastOptions } from 'react-hot-toast'
+import toastNative, { Toaster as ToasterNative } from 'react-hot-toast'
+
+type SvagToastProps = {
+  type?: 'positive' | 'negative' | 'info' | 'neutral' | 'warning'
+  position?: ToasterProps['position']
+  duration?: ToastOptions['duration'] | false
+  message: React.ReactNode
+}
+
+export const createToasterThings = ({
+  defaultPosition = 'top-center',
+  defaultDuration = 5_000,
+}: {
+  defaultPosition?: ToasterProps['position']
+  defaultDuration?: ToastOptions['duration'] | false
+} = {}) => {
+  const Toaster = () => {
+    return (
+      <ToasterNative
+        position={defaultPosition}
+        toastOptions={{
+          duration: defaultDuration || Infinity,
+          position: defaultPosition,
+        }}
+      />
+    )
+  }
+
+  const toast = ({
+    type = 'neutral',
+    position = defaultPosition,
+    duration = defaultDuration || Infinity,
+    message,
+  }: SvagToastProps) => {
+    return toastNative.custom(
+      <div>
+        {type}: {message}
+      </div>,
+      {
+        position,
+        duration: duration || Infinity,
+      }
+    )
+  }
+  Object.assign(toast, toastNative)
+
+  return {
+    Toaster,
+    toast: toast as typeof toast & typeof toastNative,
+  }
+}
