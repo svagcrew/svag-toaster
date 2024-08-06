134ba678f88db67e59f67bab81a75e97d4d9a381
Sergei Dmitriev <iserdmi@gmail.com>
2024-08-06 17:22:22 +0400

    Small fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import type React from 'react'
-import type { ToasterProps, ToastOptions } from 'react-hot-toast'
+import type { ToasterProps, ToastOptions, ToastType } from 'react-hot-toast'
 import { toast as toastNative, Toaster as ToasterNative } from 'react-hot-toast'
 
+type SvagToastPropsType = 'positive' | 'negative' | 'info' | 'neutral' | 'warning'
 type SvagToastProps = {
-  type?: 'positive' | 'negative' | 'info' | 'neutral' | 'warning'
+  type?: SvagToastPropsType
   position?: ToasterProps['position']
   duration?: ToastOptions['duration']
   message: React.ReactNode
+  render?: () => React.ReactNode
 }
 
 export const createToasterThings = ({
-  defaultPosition = 'top-center',
+  defaultPosition = 'top-right',
   defaultDuration = 5_000,
+  defaultRender,
 }: {
   defaultPosition?: ToasterProps['position']
   defaultDuration?: ToastOptions['duration']
+  defaultRender?: SvagToastProps['render']
 } = {}) => {
   const Toaster = () => {
     return (
@@ -33,16 +37,37 @@ export const createToasterThings = ({
     position = defaultPosition,
     duration = defaultDuration,
     message,
+    render = defaultRender,
   }: SvagToastProps) => {
-    return toastNative.custom(
-      <div>
-        {type}: {message}
-      </div>,
-      {
+    if (!message) {
+      return undefined
+    }
+    if (render) {
+      return toastNative.custom(
+        <div>
+          {type}: {message}
+        </div>,
+        {
+          position,
+          duration,
+        }
+      )
+    } else {
+      const nativeType = (
+        {
+          positive: 'success' as const,
+          negative: 'error' as const,
+          info: 'custom' as const,
+          neutral: 'custom' as const,
+          warning: 'error' as const,
+        } satisfies Record<SvagToastPropsType, ToastType>
+      )[type]
+
+      return toastNative[nativeType](message as any, {
         position,
         duration,
-      }
-    )
+      })
+    }
   }
   Object.assign(toast, toastNative)
 
